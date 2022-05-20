a0c20bb4e62ab1a408c476fd087e4bb0db4dc003
Alonso Villegas <av@backium.co>
2022-05-20 03:32:32 -0500

    update dev config

--- src/store/slices/design-editor/actions.ts
+++ src/store/slices/design-editor/actions.ts
@@ -1,4 +1,4 @@
-import { Page } from "./../../../interfaces/common";
+import { Page } from "~/interfaces/common";
 import { createAction } from "@reduxjs/toolkit";
 
 export const addPage = createAction<Page>("designEditor/addPage");
